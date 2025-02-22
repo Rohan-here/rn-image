c11aaae13deedcb3eeefb439634fdae9b27a9fe6
Rohan Gupta <guptarohan2002@gmail.com>
2025-02-22 17:38:02 +0530

    chore: image placeholder for test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
-import { View } from 'react-native';
+import React from 'react';
+import { Image } from 'react-native';
 
 const RNImage = () => {
-  return <View>Hi</View>;
+  return (
+    <Image
+      source={{
+        uri: 'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+      }}
+      style={{ width: 100, height: 100 }}
+    />
+  );
 };
 
 export default RNImage;
